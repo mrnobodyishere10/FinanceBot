57a245fdbbb8d9afc25274254c9731966dcaf260
mrnobodyishere10 <alvitofiqri10@gmail.com>
2026-04-06 21:17:26 +0700

    ✅ Oracle Singularity: Systematic Precision Update on [backend/src/config/featureFlags.js]

--- backend/src/config/featureFlags.js
+++ backend/src/config/featureFlags.js
@@ -1,2 +1,15 @@
 console.log("featureFlags config loaded");
-module.exports = {};
+
+const featureFlags = {
+    newFeature: false, // Set to true to enable the new feature
+    betaFeature: false, // Set to true to enable the beta feature
+};
+
+const isFeatureEnabled = (feature) => {
+    return featureFlags[feature] === true;
+};
+
+module.exports = {
+    featureFlags,
+    isFeatureEnabled,
+};
